f2c1b3c29a3342d380989a6b13b03d5b758ec14e
Mohamed Emad <mohamedemadfahmy0@gmail.com>
2024-07-16 00:18:37 +0300

    modify .env file

--- index.js
+++ index.js
@@ -169,3 +169,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
+
+const fetchAllTeachers = async () => {
+	const teachers = await Teacher.find({});
+	console.log(teachers);
+};
+fetchAllTeachers();
