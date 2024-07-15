253ef67fc8321dde055df522c195e4fdc1860977
Mohamed Emad <mohamedemadfahmy0@gmail.com>
2024-07-15 18:17:19 +0300

    edit cors settings

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use(
 	cors({
-		origin: "http://localhost:8000",
+		origin: true,
 		methods: ["GET", "POST"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 	})
